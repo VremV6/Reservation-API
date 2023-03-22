14a4fbaa5c3ac23ffaf9e26ec2d23374ae92758c
Dragos Jipa <jipadragos09@gmail.com>
2023-03-22 17:42:56 +0200

    feat: configured hot reload for nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -8,5 +11,10 @@ async function bootstrap() {
   const logger = new Logger('AppModule');
   logger.log(`Starting application on port ${port}`);
   await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
