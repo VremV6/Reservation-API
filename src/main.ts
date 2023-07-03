f1fbd5079360a9f7d25c0c2467e73b0d04209858
Dragos Jipa <jipadragos09@gmail.com>
2023-07-03 19:36:55 +0300

    feat: added cors support

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors();
   await app.listen(Constants.APP_PORT);
 
   if (module.hot) {
